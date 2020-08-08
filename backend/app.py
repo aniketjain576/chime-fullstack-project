76ea29f9639167edfd206c28f7c98db5ada0356b
aniket <aniketjain576@gmail.com>
2020-08-07 21:44:28 -0700

    made basic api for menu items with a name

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 
 # import db & models
@@ -8,8 +9,45 @@ from models import db, ExampleModel
 app = Flask(__name__.split('.')[0])
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db"
 db.init_app(app)
+ma = Marshmallow(app)
+
+#Menu Item Class
+class MenuItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __init__(self, name):
+        self.name = name
+
+#Menu Item Schema
+class MenuItemSchema(ma.Schema):
+    class Meta:
+        fields=('id', 'name')
+
+#Init schema
+menu_item_schema = MenuItemSchema()
+menu_items_schema = MenuItemSchema(many=True)
+
+#create a menu item
+@app.route('/menu-item', methods=['POST'])
+def add_menu_item():
+    name = request.json['name']
+
+    new_menu_item = MenuItem(name)
+    db.session.add(new_menu_item)
+    db.session.commit()
+
+    return menu_item_schema.jsonify(new_menu_item)
+
+#get all menu items
+@app.route('/menu-item', methods=['GET'])
+def get_menu_items():
+    all_menu_items = MenuItem.query.all()
+    result = menu_items_schema.dump(all_menu_items)
+    return jsonify(result)
+
 
 @app.route("/api/example", methods=["GET", "POST", "PUT", "DELETE"])
 def ExampleEndpoint():
